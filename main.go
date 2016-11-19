9f83651cbf8457b9a582b50625a86d729ca3488a
Joel Cornett <joel.cornett@gmail.com>
2016-11-18 20:19:52 -0500

    fixup!

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import "os"
+
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
